77c5c175953a24c2916ba587700b60f0bf7556ff
Q-QJB <447571964@qq.com>
2021-04-10 22:20:02 +0800

    2

---
+++ 21-4-10/HelloWord.java
@@ -0,0 +1,20 @@
+
+
+//如果一个类是public的 那么这个类的类名 一定要和文件名相同,
+//一个java文件当中只能有一个类
+public class/*类*/ HelloWord/*类名*/ {//类体
+
+	public static void main/*方法*/(String[] args) {/*方法体*/
+		
+		System.out.println("HelloWord"/*字符串*/);
+	}
+}
+//原码文件->编译->字节码文件->运行
+//JDK->java开发工具
+//JRE->java运行环境
+//JVE->java虚拟机
+
+//注释     
+//行注释    //   -> ctr+/
+//块注释   /**/  -> ctr+shift+/
+//文档注释 /** */ 
